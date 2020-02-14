287e24968db34ce9dd18b1d6428b16534967b0b6
David Dueber <ddueber@gmail.com>
2020-02-14 08:43:50 -0500

    Fix expected value and delta item mean to work properly with categorical variables. Breaks everything else!!

--- R/MeasEquiv_EffectSize_Base.R
+++ R/MeasEquiv_EffectSize_Base.R
@@ -115,18 +115,29 @@ item_dmacs <- function (LambdaR, ThreshR,
 #' \code{\link{mplus_dmacs}}, which are the only functions in this
 #' package intended for casual users
 #'
-#' @param LambdaR is the factor loading of the item onto the factor of
+#' @param LambdaR is the factor loading of the indicator onto the factor of
 #' interest for the reference group.
-#' @param ThreshR is the indicator intercept (for continuous
-#' indicators) or a vector of thresholds (for
-#' categorical indicators) for the reference group.
-#' @param LambdaF is the factor loading of the item onto the factor of
+#' @param LambdaF is the factor loading of the indicator onto the factor of
 #' interest for the focal group.
-#' @param ThreshF is the indicator intercept (for continuous
-#' indicators) or a vector of thresholds (for
-#' categorical indicators) for the focal group.
+#' @param NuR is the indicator intercept for the reference group.
+#' @param NuF is the indicator intercept for the focal group.
 #' @param MeanF is the factor mean in the focal group
 #' @param VarF is the factor variances in the focal group.
+#' @param ThreshR is a vector of thresholds (for categorical indicators)
+#' for the reference group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param ThreshR is a vector of thresholds (for categorical indicators)
+#' for the reference group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param ThreshF is a vector of thresholds (for categorical indicators)
+#' for the focal group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param ThetaR is the indicator residual variance in the
+#' reference group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param ThetaF is the indicator residual variance in the
+#' focal group. Defaults to \code{NULL} for continuous
+#' indicators.
 #' @param categorical is a Boolean variable declaring whether the variables
 #' in the model are ordered categorical. Models in which some variables are
 #' categorical and others are continuous are not supported. If no value is
@@ -143,11 +154,11 @@ item_dmacs <- function (LambdaR, ThreshR,
 #' @examples
 #' LambdaF <- 0.74
 #' LambdaR <- 0.76
-#' ThreshF <- 1.28
-#' ThreshR <- 0.65
+#' NuF     <- 1.28
+#' NuR     <- 0.65
 #' MeanF   <- 0.21
 #' VarF    <- 1.76
-#' delta_mean_item(LambdaR, ThreshR, LambdaF, ThreshF, MeanF, VarF)
+#' delta_mean_item(LambdaR, LambdaF, NuR, NuF, MeanF, VarF)
 #'
 #' @section References:
 #' Nye, C. & Drasgow, F. (2011). Effect size indices for analyses of
@@ -159,26 +170,34 @@ item_dmacs <- function (LambdaR, ThreshR,
 #' @importFrom stats dnorm
 #' @importFrom stats integrate
 
-delta_mean_item <- function (LambdaR, ThreshR,
-                             LambdaF, ThreshF,
+delta_mean_item <- function (LambdaR, LambdaF,
+                             NuR, NuF,
                              MeanF, VarF,
+                             ThreshR = NULL, ThreshF = NULL,
+                             ThetaR = NULL, ThetaF = NULL,
                              categorical = FALSE) {
+  # Use Thresholds as a check for categorical-ness
+  if (length(ThreshR) > 1) {
+    categorical <- TRUE
+    ## If threshold vectors do not have the same length, throw an error
+    if (length(ThreshR) != length(ThreshF)) stop("Item must have same number of thresholds in both reference and focal group")
 
-  ## If threshold vectors do not have the same length, throw an error
-  if (length(ThreshR) != length(ThreshF)) stop("Item must have same number of thresholds in both reference and focal group")
+  }
 
   ## If item does not load on factor, return NA
   if(LambdaR == 0) {return(NA)}
 
-  ## if more than one threshold, we must be in a categorical situation
-  if (length(ThreshR) > 1) { categorical <- TRUE}
-
   ## Create a function for the integrand using the expected value function expected_value
-  ## The sqrt(VarF) is there because we did a change of varianbles into the z metric
-  integrand <- function(z, LambdaR, LambdaF, ThreshR, ThreshF, MeanF, VarF, categorical) {
+  ## The sqrt(VarF) is there because we did a change of variables into the z metric.
+  ## Probably would have been easier to fix the dnorm, but it's done.
+  integrand <- function(z, LambdaR, LambdaF,
+                           NuR, NuF,
+                           ThreshR, ThreshF,
+                           ThetaR, ThetaF,
+                           MeanF, VarF, categorical) {
 
-    (expected_value(LambdaF, ThreshF, MeanF+z*sqrt(VarF), categorical) -
-       expected_value(LambdaR, ThreshR, MeanF+z*sqrt(VarF), categorical)) * dnorm(z) * sqrt(VarF)
+    (expected_value(LambdaF, NuF, MeanF+z*sqrt(VarF), ThreshF, ThetaF, categorical) -
+       expected_value(LambdaR, NuR, MeanF+z*sqrt(VarF), ThreshR, ThetaR, categorical)) * dnorm(z) * sqrt(VarF)
 
   }
   ## Now, integrate
@@ -231,7 +250,7 @@ delta_mean_item <- function (LambdaR, ThreshR,
 delta_var <- function (LambdaR, LambdaF, VarF, categorical = FALSE) {
 
   if(categorical) {
-    warning("Delta variance can only be computed for linear models, not for categorical ones")
+    warning("At this time, delta variance can only be computed for linear models, not for categorical ones")
     return(NULL)
   }
   delta_cov_mat <- matrix(nrow=length(LambdaR), ncol=length(LambdaR))
@@ -253,44 +272,44 @@ delta_var <- function (LambdaR, LambdaF, VarF, categorical = FALSE) {
 #' item parameters and factor value.
 #'
 #' @param Lambda is the loading of the indicator on the factor.
-#' @param Thresh is the intercept (continuous indicator) or vector of
-#' thresholds (categorical indicator) for the indicator.
-#' @param Theta is the value of the factor for which the expected value is
+#' @param Nu is the indicator intercept.
+#' @param Eta is the value of the factor for which the expected value is
 #' to be computed
+#' @param Thresh is the vector of thresholds for a categorical indicator.
+#' Defaults to NULL as \code{Thresh} is not needed for continuous indicators.
 #' @param categorical is a Boolean variable declaring whether the variables
 #' in the model are ordered categorical. Models in which some variables are
 #' categorical and others are continuous are not supported. If no value is
-#' provided, categorical defaults to \code{FALSE}, although if multiple
-#' thresholds are provided for an item, categorical will be forced to
-#' \code{TRUE}. A graded response model with probit link (e.g., DWLS in
-#' lavaan or WLSMV in Mplus) is used for categorical variables. If you desire
-#' for other categorical models (e.g., IRT parameterization) to be supported,
-#' e-mail the maintainer.
+#' provided, categorical defaults to \code{FALSE}. A graded response model
+#' with probit link (e.g., DWLS in lavaan or WLSMV in Mplus) is used for
+#' categorical variables. If you desire for other categorical models
+#' (e.g., IRT parameterization) to be supported, e-mail the maintainer.
 #'
-#' @return The expected value of the indicator when the factor score is \code{Theta}
+#' @return The expected value of the indicator when the factor score is \code{Eta}
 #' @keywords internal
 #' @importFrom stats pnorm
 
-expected_value <- function (Lambda, Thresh, Theta, categorical = FALSE) {
+expected_value <- function (Lambda, Nu, Eta, Thresh = NULL, Theta = NULL, categorical = FALSE) {
 
-  ## if more than one threshold, we must be in a categorical situation
-  if (length(Thresh) > 1) { categorical <- TRUE }
+  ## if Thresholds are provided, we must be in a categorical situation
+  if (!is.null(Thresh)) { categorical <- TRUE }
 
   if (categorical) {
     ## Graded Response model with probit link.
     ## categories go from 0 to number of thresholds
     max <- length(Thresh)
     ## let's be lazy and make a max+1 category that is impossible to attain
-    Thresh[max+1] <- 9999999
+    Thresh[max+1] <- Inf
 
     ## I know for loops are bad, but using both current and next element of vector in sapply is hard for me!
     expected <- 0
-    for (i in 1:max) {expected <- expected + i*(pnorm(Lambda*(Theta-Thresh[i]))-pnorm(Lambda*(Theta-Thresh[i+1])))}
+    for (i in 1:max) {expected <- expected + i*(pnorm(Thresh[i+1]-Nu-Lambda*Eta, sd = sqrt(Theta)) -
+                                                  pnorm(Thresh[i]-Nu-Lambda*Eta, sd = sqrt(Theta)))}
     expected
 
   } else {
     ## The linear continuous world is so easy!!
-    Thresh+Lambda*Theta
+    Nu+Lambda*Eta
   }
 
 }
