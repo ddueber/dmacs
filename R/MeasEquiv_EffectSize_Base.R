b9871bc79e283346e03ba282deaf19768d6309c9
David Dueber <ddueber@gmail.com>
2020-02-18 11:56:29 -0500

    Add DTF plot function

--- R/MeasEquiv_EffectSize_Base.R
+++ R/MeasEquiv_EffectSize_Base.R
@@ -335,3 +335,160 @@ expected_value <- function (Lambda, Nu, Eta, Thresh = NULL, Theta = NULL, catego
 
 }
 
+
+
+
+
+
+
+
+#' Expected bias in test score
+#'
+#' \code{DTF_graph} produces a graph of expected total score or of expected
+#' bias in total score based on item parameters in the reference group and
+#' based on item parameters in the focal group.
+#'
+#' \code{DTF_graph} is called by \code{dmacs_summary}, which
+#' in turn is called by \code{\link{lavaan_dmacs}} and
+#' \code{\link{mplus_dmacs}}, which are the only functions in this
+#' package intended for casual users
+#'
+#' @param LambdaR is the factor loading of the indicator onto the factor of
+#' interest for the reference group.
+#' @param LambdaF is the factor loading of the indicator onto the factor of
+#' interest for the focal group.
+#' @param NuR is the indicator intercept for the reference group.
+#' @param NuF is the indicator intercept for the focal group.
+#' @param MeanF is the factor mean in the focal group
+#' @param VarF is the factor variances in the focal group.
+#' @param ThreshR is a vector of thresholds (for categorical indicators)
+#' for the reference group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param ThreshF is a vector of thresholds (for categorical indicators)
+#' for the focal group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param ThetaR is the indicator residual variance in the
+#' reference group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param ThetaF is the indicator residual variance in the
+#' focal group. Defaults to \code{NULL} for continuous
+#' indicators.
+#' @param categorical is a Boolean variable declaring whether the variables
+#' in the model are ordered categorical. Models in which some variables are
+#' categorical and others are continuous are not supported. If no value is
+#' provided, categorical defaults to \code{FALSE}, although if a vector of
+#' thresholds are provided, categorical will be forced to
+#' \code{TRUE}. A graded response model with probit link (e.g., DWLS in
+#' lavaan or WLSMV in Mplus) is used for categorical variables. If you desire
+#' for other categorical models (e.g., IRT parameterization) to be supported,
+#' e-mail the maintainer.
+#' @param minEta is the minimum factor score to be considered. Defaults to -3.
+#' @param maxEta is the maximum factor score to be considered. Defaults to -3.
+#'
+#' @return A graph of expected total score or bias in expected total score due to
+#' measurement nonequivalence, per Nye & Drasgow (2011).
+#'
+#' @examples
+#' LambdaF <- matrix(c(1.00, 0.74,  1.14, 0.92), ncol = 1)
+#' LambdaR <- matrix(c(1.00, 0.76,  1.31, 0.98), ncol = 1)
+#' NuF     <- c(0.00, 1.28, -0.82, 0.44)
+#' NuR     <- c(0.00, 0.65, -0.77, 0.47)
+#' DTF_graph(LambdaR, LambdaF, NuR, NuF)
+#'
+#' @section References:
+#' Nye, C. & Drasgow, F. (2011). Effect size indices for analyses of
+#' measurement equivalence: Understanding the practical importance of
+#' differences between groups. \emph{Journal of Applied Psychology, 96}(5),
+#' 966-980.
+#'
+#' @export
+#' @importFrom stats reshape
+
+DTF_graph       <- function (LambdaR, LambdaF,
+                             NuR, NuF,
+                             RefGroup, FocGroup,
+                             ThreshR = NULL, ThreshF = NULL,
+                             ThetaR = NULL, ThetaF = NULL,
+                             categorical = FALSE,
+                             minEta = -3, maxEta = 3,
+                             GraphType = "Scores") {
+
+  ## Use Thresholds as a check for categorical-ness
+  if (categorical) {
+    categorical <- TRUE
+    ## If threshold vectors do not have the same length, throw an error
+    for (i in 1:length(ThreshR)) {
+      if (length(ThreshR[[i]]) != length(ThreshF[[i]])) stop("Item must have same number of thresholds in both reference and focal group")
+    }
+  }
+
+  ## Vector of Eta values to compute expected scores or biases
+  Eta <- seq(minEta, maxEta, length.out = 100)
+
+  ## iterate over the items to create expected scores
+  # reference group
+  ref_expected <- sapply(1:length(LambdaF), function (x) {
+    mapply(expected_value, Eta = Eta,  MoreArgs = list(Lambda = LambdaR[x], Nu = NuR[x],
+                                                       Thresh = ThreshR[[x]], Theta = ThetaR[[x]],
+                                                       categorical = categorical))
+  })
+  # total score
+  ref_expected_total <- rowSums(ref_expected)
+
+
+  # focal group
+  foc_expected <- sapply(1:length(LambdaF), function (x) {
+    mapply(expected_value, Eta = Eta,  MoreArgs = list(Lambda = LambdaF[x], Nu = NuF[x],
+                                                       Thresh = ThreshF[[x]], Theta = ThetaF[[x]],
+                                                       categorical = categorical))
+  })
+  # total score
+  foc_expected_total <- rowSums(foc_expected)
+
+  #bias
+  bias_expected <- foc_expected_total - ref_expected_total
+
+  # merge into a dataframe for graphing with
+  graph_data <- data.frame(list(Eta = Eta, Ref = ref_expected_total, Foc = foc_expected_total, Bias = bias_expected))
+
+
+  if (GraphType == "Scores") {
+
+    # make it long so that we can confrom to Hadley's ridiculous standards
+    graph_data_long <- reshape(graph_data[,1:3], varying = list(2:3), direction = "long")
+    colnames(graph_data_long) <- c("Eta", "Group", "Expected", "id")
+    graph_data_long$Group[graph_data_long$Group == 1] <- RefGroup
+    graph_data_long$Group[graph_data_long$Group == 2] <- FocGroup
+
+    ## Plot both sets of scores
+    ggplot2::ggplot(graph_data_long, aes(x = Eta)) +
+                    geom_line(aes(y = Expected, group = Group, color = Group)) +
+                    scale_colour_manual(values=c("red","blue")) +
+                    ggtitle("Expected Total Scores") +
+                    xlab("Factor Score") +
+                    ylab("Expected Total Score") +
+                    theme(plot.title = element_text(hjust = 0.5), legend.position =  c(0.9, 0.2)) +
+                    scale_x_continuous(limits=c(minEta, maxEta), breaks=seq(minEta,maxEta,1))
+
+  } else {
+    # Plot the bias
+    ggplot2::ggplot(graph_data, aes(x = Eta)) +
+                    geom_line(aes(y = Bias), colour = "red") +
+                    ggtitle(paste0("Expected Bias between ", FocGroup, " and ", RefGroup)) +
+                    xlab("Factor Score") +
+                    ylab("Expected Bias") +
+                    theme(plot.title = element_text(hjust = 0.5)) +
+                    scale_x_continuous(limits=c(minEta, maxEta), breaks=seq(minEta,maxEta,1))
+  }
+
+
+  expected_value <- function (Lambda, Nu, Eta, Thresh = NULL, Theta = NULL, categorical = FALSE) {}
+}
+
+
+
+
+LambdaF <- matrix(c(1.00, 0.74,  1.14, 0.92), ncol = 1)
+LambdaR <- matrix(c(1.00, 0.76,  1.31, 0.98), ncol = 1)
+NuF     <- c(0.00, 1.28, -0.82, 0.44)
+NuR     <- c(0.00, 0.65, -0.77, 0.47)
